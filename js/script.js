c245251f60a69140d8ca9835d7c5f499714d1ceb
VVT <example@gmail.com>
2025-12-23 00:47:29 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// const answers = [];
+
+// // answers[0] = prompt('Your name?', '');
+// // answers[1] = prompt('Your age?', '');
+// // answers[2] = prompt('Your country?', '');
+
+// // document.write(answers);
+
+// console.log(typeof(answers));
+
+console.log(2 + 2 * 2 !== '6');
