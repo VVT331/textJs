1406faf59f1ff3e150c5ee49277a503e855378cc
VVT <example@gmail.com>
2025-12-28 23:18:22 +0300

    update

--- js/script.js
+++ js/script.js
@@ -8,9 +8,33 @@ let personalMovieDB = {
     privat: false
 };
 
-let lastFilms = prompt('Один из последних просмотренных фильмов?', '');
-let estimationLastFilms = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastFilms] = estimationLastFilms;
+
+for (let i = 0; i < 2; i++) {
+    let lastFilms = prompt('Один из последних просмотренных фильмов?', ''),
+        estimationLastFilms = +prompt('На сколько оцените его?', '');
+
+    if (lastFilms != null && estimationLastFilms != null && lastFilms != '' && estimationLastFilms != '' && lastFilms.length < 50) {
+        personalMovieDB.movies[lastFilms] = estimationLastFilms;
+    } else {
+        i--;
+    }
+}
+
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы - классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы - киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
 
 console.log(personalMovieDB);
+
+
+
+
